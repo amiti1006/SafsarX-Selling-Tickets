56b21f0fcb0f1c71c67366e822b686ae33ab0d6d
Maor002 <108116035+Maor002@users.noreply.github.com>
2024-03-17 11:39:39 +0200

    driver.py

--- test_selling_tickets/tests.py
+++ test_selling_tickets/tests.py
@@ -14,56 +14,7 @@ my_driver = webdriver_instance()
 my_driver.get(base_url)
 actions = Actions(my_driver)
 
-
-@pytest.mark.parametrize("expected_text", ['הרשמה', 'יש לי חשבון', 'מכירת כרטיסים\nבראש שקט', 'שנתחיל בלהכיר ?'])
-def test_navigation(expected_text):
-    # elements
-    elements = page_start_screeen(actions)
-    assert all(expected_text in elements for element in elements)
-    my_driver.get(base_url)
-    actions.update_driver(my_driver)
-
-
-@pytest.mark.skip
-def test_success_screen():  # לא נבדק
-    my_driver.get(base_url)
-    page_start_screeen(actions)
-    register_page(actions, 'youuppiiiik@gmail.com', 'שדג', 'דשגשדג', '0544425781', '545879541')
-    subtitle = actions.find_element((By.XPATH, "//p[contains(text(), 'אחרי שתמלאו את כל הפרטים')]"))
-    assert 'אחרי שתמלאו את כל הפרטים' in subtitle.text
-    my_driver.get(base_url)
-    actions.update_driver(my_driver)
-
-
-def test_existing_user_move_to_sign_in():
-    my_driver.get(base_url)
-    navigation_to_selling_tickets(actions)
-    log_in(actions, '0533363708')
-    category_name_field = actions.find_element((By.XPATH, '//*[@id="eventInfoForm"]/div[1]/div/label'))
-    assert category_name_field.text == 'באיזו קטגוריה הכרטיסים?'
-    my_driver.get(base_url)
-    actions.update_driver(my_driver)
-
-
-def test_moveTicket_sale_verification():
-    my_driver.get(base_url)
-    navigation_to_selling_tickets(actions)
-    log_in(actions, '0533363708')
-    selling_tickets_button = actions.find_element(safsarX_selling_Tickets.data_of_elements.data.selling_tickets_button)
-    actions.click_element(selling_tickets_button)
-    assert actions.driver.current_url == 'url of this page'
-
-
-@pytest.mark.parametrize("expected_text", ['Header', 'Title & subtitle', 'Text field', 'כניסה'])
-def test_Verification_Screen_structure(expected_text):
-    test_moveTicket_sale_verification()
-    elements = page_verification_screeen(actions, 'xpathHeader', 'xpath_Title', 'xpath_subtitle', 'xpath_Text_field',
-                                         'xpath_button')
-    assert all(expected_text in elements for element in elements)
-    my_driver.get(base_url)
-    actions.update_driver(my_driver)
-
-
+#sanity
 def test_full_process_selling():
     navigation_to_selling_tickets(actions)
     log_in(actions, '0533363708')
@@ -79,6 +30,7 @@ def test_full_process_selling():
     for x in paragraphs:
         print(x)
     assert any('מכירת הכרטיסים שלך נשלחה לאישור' in paragraph for paragraph in paragraphs)
+
 
 
 # test 1
@@ -179,3 +131,54 @@ def test_bank_name_is_valid(accountNumber_text, expected_accountNumber_text):
         print(display_price, 'הוכנס מספר בנק שגוי', e)
     my_driver.get(base_url)
     actions.update_driver(my_driver)
+
+
+@pytest.mark.parametrize("expected_text", ['הרשמה', 'יש לי חשבון', 'מכירת כרטיסים\nבראש שקט', 'שנתחיל בלהכיר ?'])
+def test_navigation(expected_text):
+    # elements
+    elements = page_start_screeen(actions)
+    assert all(expected_text in elements for element in elements)
+    my_driver.get(base_url)
+    actions.update_driver(my_driver)
+
+
+@pytest.mark.skip
+def test_success_screen():  # לא נבדק
+    my_driver.get(base_url)
+    page_start_screeen(actions)
+    register_page(actions, 'youuppiiiik@gmail.com', 'שדג', 'דשגשדג', '0544425781', '545879541')
+    subtitle = actions.find_element((By.XPATH, "//p[contains(text(), 'אחרי שתמלאו את כל הפרטים')]"))
+    assert 'אחרי שתמלאו את כל הפרטים' in subtitle.text
+    my_driver.get(base_url)
+    actions.update_driver(my_driver)
+
+
+def test_existing_user_move_to_sign_in():
+    my_driver.get(base_url)
+    navigation_to_selling_tickets(actions)
+    log_in(actions, '0533363708')
+    category_name_field = actions.find_element((By.XPATH, '//*[@id="eventInfoForm"]/div[1]/div/label'))
+    assert category_name_field.text == 'באיזו קטגוריה הכרטיסים?'
+    my_driver.get(base_url)
+    actions.update_driver(my_driver)
+
+
+def test_moveTicket_sale_verification():
+    my_driver.get(base_url)
+    navigation_to_selling_tickets(actions)
+    log_in(actions, '0533363708')
+    selling_tickets_button = actions.find_element(safsarX_selling_Tickets.data_of_elements.data.selling_tickets_button)
+    actions.click_element(selling_tickets_button)
+    assert actions.driver.current_url == 'url of this page'
+
+
+@pytest.mark.parametrize("expected_text", ['Header', 'Title & subtitle', 'Text field', 'כניסה'])
+def test_Verification_Screen_structure(expected_text):
+    test_moveTicket_sale_verification()
+    elements = page_verification_screeen(actions, 'xpathHeader', 'xpath_Title', 'xpath_subtitle', 'xpath_Text_field',
+                                         'xpath_button')
+    assert all(expected_text in elements for element in elements)
+    my_driver.get(base_url)
+    actions.update_driver(my_driver)
+
+
